563fd244b943d9ff7108ff821c67a174027b959d
Pakorn Tawansang <pagorn56110@gmail.com>
2019-11-28 13:03:34 +0700

    Revert to last success1

--- Scripts/Homepage/F01_SC011/F0111-TC009/Script1574913043787.groovy
+++ Scripts/Homepage/F01_SC011/F0111-TC009/Script1574913043787.groovy
@@ -15,3 +15,34 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.up.ac.th/th/')
+
+WebUI.maximizeWindow()
+
+WebUI.delay(3)
+
+WebUI.click(findTestObject('แสดงผลแถบเมนู/F02_SC004/F0204-TC001/นิสิตปัจจุบัน'))
+
+WebUI.delay(1)
+
+WebUI.click(findTestObject('แสดงผลแถบเมนู/F02_SC004/F0204-TC007 and F0204-TC008/ทุนการศึกษา'))
+
+WebUI.verifyElementPresent(findTestObject('แสดงผลแถบเมนู/F02_SC004/F0204-TC007 and F0204-TC008/กองกิจการนิสิต'), 20)
+
+WebUI.verifyElementText(findTestObject('แสดงผลแถบเมนู/F02_SC004/F0204-TC007 and F0204-TC008/กองกิจการนิสิต'), 'กองกิจการนิสิต')
+
+WebUI.verifyElementClickable(findTestObject('แสดงผลแถบเมนู/F02_SC004/F0204-TC007 and F0204-TC008/กองกิจการนิสิต'))
+
+WebUI.verifyElementPresent(findTestObject('แสดงผลแถบเมนู/F02_SC004/F0204-TC007 and F0204-TC008/กองบริการการศึกษา'), 20)
+
+WebUI.verifyElementText(findTestObject('แสดงผลแถบเมนู/F02_SC004/F0204-TC007 and F0204-TC008/กองบริการการศึกษา'), 'กองบริการการศึกษา')
+
+WebUI.verifyElementClickable(findTestObject('แสดงผลแถบเมนู/F02_SC004/F0204-TC007 and F0204-TC008/กองบริการการศึกษา'))
+
+WebUI.delay(3)
+
+'ปิดหน้าเว็บ'
+WebUI.closeBrowser()
+
--- Scripts/แสดงผลแถบเมนู/F02_SC001/F0201-TC013/Script1574822669960.groovy
+++ Scripts/แสดงผลแถบเมนู/F02_SC001/F0201-TC013/Script1574822669960.groovy
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
@@ -36,4 +37,43 @@ WebUI.delay(1)
 //Check personnel page
 WebUI.verifyElementPresent(findTestObject('แสดงผลแถบเมนู/F02_SC001/F0201-TC013/หน้า ประมวลจริยธรรม'), 20)
 WebUI.delay(1)
+=======
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+'แสดงหน้า หน้าจอหลัก'
+WebUI.openBrowser(GlobalVariable.G_url)
+WebUI.maximizeWindow()
+WebUI.delay(1)
+
+'แสดงปุ่มต่อจากแถบเมนู "เกี่ยวกับเรา"'
+WebUI.click(findTestObject('Object Repository/แสดงผลแถบเมนู/F02_SC001/F0201-TC004/เกี่ยวกับเรา'))
+WebUI.delay(1)
+///Check  Menu personnel 1
+WebUI.verifyTextPresent(GlobalVariable.G_Culture, false)
+WebUI.delay(2)
+
+WebUI.click(findTestObject('Object Repository/แสดงผลแถบเมนู/F02_SC001/F0201-TC012/วัฒนธรรมองค์กร tab'))
+WebUI.delay(1)
+
+WebUI.click(findTestObject('Object Repository/แสดงผลแถบเมนู/F02_SC001/F0201-TC013/ประมวลจริยธรรม และคุณธรรมของบุคลากร'))
+WebUI.delay(1)
+
+//Check personnel page
+WebUI.verifyElementPresent(findTestObject('แสดงผลแถบเมนู/F02_SC001/F0201-TC013/หน้า ประมวลจริยธรรม'), 20)
+WebUI.delay(1)
+>>>>>>> parent of 9fd5857... Rename the folder to Navbar
 WebUI.closeBrowser()
